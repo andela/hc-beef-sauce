ffeac70a472b53e39972ff9fba160e988bfe2636
Tapiwa Lason Mapfundematsva <mapfundetl@gmail.com>
2018-07-10 16:11:06 +0300

    [#158532839] add 3rd party livechat plugin

--- static/js/help.js
+++ static/js/help.js
@@ -6,3 +6,6 @@ function myFunction(id) {
         x.className = x.className.replace(" w3-show", "");
     }
 }
+
+// Code for Live Chat Pop Up (Pure Chat)
+window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: 'fd74712b-e410-48d2-9072-9376d07ad5c7', f: true }); done = true; } }; })();
