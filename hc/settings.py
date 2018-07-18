3c74f7c7a9c9aabc1e6cc6890a8dda6631e79a9f
brandeddavid <david.mathenge98@gmail.com>
2018-07-18 17:06:57 +0300

    [FT #159000976]Add email ssl

--- hc/settings.py
+++ hc/settings.py
@@ -160,8 +160,8 @@ EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
-# EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL")
+# EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL")
 
 # Twilio credentials
 ACCOUNT_SID = os.environ.get("ACCOUNT_SID")
