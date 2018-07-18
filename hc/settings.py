382506b6b02260caf01929d38913c1c0c80af760
brandeddavid <david.mathenge98@gmail.com>
2018-07-18 13:32:02 +0300

    [FT #159000976]Add SMTP setup in setting

--- hc/settings.py
+++ hc/settings.py
@@ -149,6 +149,17 @@ PUSHOVER_EMERGENCY_EXPIRATION = 86400
 PUSHBULLET_CLIENT_ID = None
 PUSHBULLET_CLIENT_SECRET = None
 
+# SMTP Set up
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
+DJMAIL_REAL_BACKEND = os.environ.get("DJMAIL_REAL_BACKEND")
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL")
+
 if os.path.exists(os.path.join(BASE_DIR, "hc/local_settings.py")):
     from .local_settings import *
 else:
